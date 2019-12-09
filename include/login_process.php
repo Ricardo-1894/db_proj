771a89461ff02d722d7262cf077da257d054b3cd
Zhp <Ricardo_1894@outlook.com>
2019-12-09 15:17:43 -0500

    log_inprocess

--- include/login_process.php
+++ include/login_process.php
@@ -1,17 +1,17 @@
 <?php
 session_start();
 
-include_once 'dbh.inc.php';
+include_once "dbh.inc.php";
 
 $user_name = $_POST['user_name'];
 $password = $_POST['password'];
 if($user_name == null){
     echo "<script>alert('UserName cannot leave blank!');
-        location.href='/php_lessons/login.php';</script>";
+        location.href='../login.php';</script>";
 }
 if($password == null){
     echo "<script>alert('Password cannot leave blank!');
-        location.href='/php_lessons/login.php';</script>";
+        location.href='../login.php';</script>";
 }
 
 $sql = "SELECT * FROM user WHERE uid=? and upwd=?";
@@ -22,7 +22,7 @@ if(mysqli_stmt_prepare($stmt,$sql)){
     $result = mysqli_stmt_get_result($stmt);
     if(mysqli_num_rows($result) == 0){
         echo "<script>alert('Invalid username or password, try again!');
-            location.href='/php_lessons/login.php';</script>";
+            location.href='../login.php';</script>";
     }
     else{
         $_SESSION['username'] = $user_name;
