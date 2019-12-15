cfabf4b6acae294c817dae6e67fa3439a600a4d1
zzwang1996 <zzwang1996@gmail.com>
2019-12-15 03:24:23 -0500

    search function added

--- include/search_process.php
+++ include/search_process.php
@@ -1,64 +1,63 @@
 <?php
 include 'dbh.inc.php';
-
-$res = array(
-	'block' => array(),
-	'hood' => array(),
-	'thread' => array(),
-);
-
-try {
-	$res['data'] = getWeatherStations();
-} catch (Exception $e) {
-	$res['status'] = -1;
-	$res['message'] = $e->getMessage();
-} finally {
-	echo json_encode($res);
-}
-
-function get_block(){
-    ##
-}
-function get_friend(){
-    ##
-}
-function get_thread(){
-    ##
-}
-
-session_start();
 $conn = connect_database();
-$user_id = $_SESSION['username'];
+$user_id = $_POST['username'];
+$keyword = "%".$_POST['keyword']."%";
 
-$splited = array(explode('#', $_POST['search_string']);
-if($splited[1] == 'block'){
-    $sql = "
-    SELECT * as f_num
-    FROM user natural join thread_users natural join thread join msg
-    WHERE uid = ? and thread_type = 0
-    and thread.last_msg_id = msg.msg_id
-    and msg.timestamp > user.last_logout_time";
-}
-else if($splited[1] == 'hood'){
-    ##
-}
-else{
-    
-}
-
-
-
-$sql = "
-    SELECT count(*) as f_num
-    FROM user natural join thread_users natural join thread join msg
-    WHERE uid = ? and thread_type = 0
-    and thread.last_msg_id = msg.msg_id
-    and msg.timestamp > user.last_logout_time";
-$stmt = mysqli_stmt_init($conn);
-if(mysqli_stmt_prepare($stmt,$sql)){
-    mysqli_stmt_bind_param($stmt, "s", $user_id);
+function search_block(){
+    global $conn, $user_id, $keyword;
+    $sql = "SELECT block_name, COUNT(uid)
+            FROM block NATURAL JOIN user 
+            WHERE block_name like ? and user.bid = block.bid
+            GROUP BY block_name";
+    $stmt = mysqli_stmt_init($conn);
+    if(mysqli_stmt_prepare($stmt,$sql)){
+    mysqli_stmt_bind_param($stmt, "ss", $user_id);
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
+    }
+    $dbdata = array();
+    while ( $row = $result->fetch_assoc())  {
+        $dbdata[]=$row;
+    }
+    $db_encode = json_encode($dbdata);
+    echo $db_encode;
 }
-$data = mysqli_fetch_assoc($result);
-$f_num = $data['f_num'];
+
+function search_friend(){
+    global $conn, $user_id, $keyword;
+    $sql = "SELECT unickname, textbody, timestamp, msg_id
+            FROM user NATURAL JOIN thread_users NATURAL JOIN thread NATURAL JOIN thread_msgs NATURAL JOIN msg, user_profile
+            WHERE user.uid = ? AND thread_type = 0 AND textbody LIKE ? AND user_profile.uid = author";
+    $stmt = mysqli_stmt_init($conn);
+    if(mysqli_stmt_prepare($stmt,$sql)){
+    mysqli_stmt_bind_param($stmt, "ss", $user_id);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    }
+    $dbdata = array();
+    while ( $row = $result->fetch_assoc())  {
+        $dbdata[]=$row;
+    }
+    $db_encode = json_encode($dbdata);
+    echo $db_encode;
+}
+function serach_thread(){
+    global $conn, $user_id, $keyword;
+    $sql = "SELECT thread_name, author, textbody, timestamp
+            FROM user NATURAL JOIN thread_users NATURAL JOIN thread NATURAL JOIN msg
+            WHERE uid = ? and msg_id = last_msg_id and thread_name like ?";
+    $stmt = mysqli_stmt_init($conn);
+    if(mysqli_stmt_prepare($stmt,$sql)){
+    mysqli_stmt_bind_param($stmt, "ss", $user_id);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    }
+    $dbdata = array();
+    while ( $row = $result->fetch_assoc())  {
+        $dbdata[]=$row;
+    }
+    $db_encode = json_encode($dbdata);
+    echo $db_encode;
+}
+?>
